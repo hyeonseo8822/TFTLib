6f15db53aa3747e66225cc19a6f4e4bef81a8564
조현서 <hyeonseojo929@gmail.com>
2026-06-13 18:34:05 +0900

    fix:시너지 사이드바 수정

--- src/components/synergy/SynergyDetailPanel.jsx
+++ src/components/synergy/SynergyDetailPanel.jsx
@@ -5,8 +5,7 @@ import { SynergyTierEffects } from './SynergyTierEffects'
 
 export function SynergyDetailPanel({ synergy }) {
   return (
-    <aside className="w-full md:w-96 p-margin bg-surface-container-low flex flex-col gap-lg overflow-y-auto max-h-screen custom-scrollbar sticky top-20">
-      <SynergyDetailHeader synergy={synergy} />
+    <aside className="w-full md:w-[1000px] p-margin bg-surface-container-low flex flex-col gap-lg overflow-y-auto max-h-screen custom-scrollbar sticky top-20">      <SynergyDetailHeader synergy={synergy} />
       <SynergyTierEffects effects={synergy.tierEffects} />
       <SynergyChampionGrid champions={synergy.champions} />
       <SynergyTacticalNote note={synergy.tacticalNote} />
